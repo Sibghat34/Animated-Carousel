ae35a9349f1d5216a7bc6c864b78cf8b37c8daff
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-11-02 04:08:56 +0500

    framer motion installed and pictures added

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {useState} from 'react';
+import {motion} from 'framer-motion';
+
+import image from '@/public/assets/images/image1.jpg';
+import imag2 from '@/public/assets/images/image2.jpg';
+import imag3 from '@/public/assets/images/image3.jpg';
+import imag4 from '@/public/assets/images/image4.jpg';
 
 const ImageSlider = () => {
+
+    const [positonIndex, setPositonIndex] = useState([0, 1, 2, 3, 4]);
+
   return (
     <div></div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import ImageSlider from './components/ImageSlider'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <ImageSlider/>
     </main>
   )
 }
