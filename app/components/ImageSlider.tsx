15b5e48d5ae560c0f06f2a94f088356a47db1910
Sibghat Ullah <sibghat.aamir34@gmail.com>
2023-11-02 05:20:52 +0500

    all issues resolved

--- app/components/ImageSlider.tsx
+++ app/components/ImageSlider.tsx
@@ -1,55 +1,71 @@
-"use client";
+'use client'
 
-import React from "react";
-import { useState } from "react";
-import { motion } from "framer-motion";
+import React from 'react'
+import {useState} from 'react';
+import {motion} from 'framer-motion';
 
-import image from "@/public/assets/images/image1.jpg";
-import imag2 from "@/public/assets/images/image2.jpg";
-import imag3 from "@/public/assets/images/image3.jpg";
-import imag4 from "@/public/assets/images/image4.jpg";
-import imag5 from "@/public/assets/images/image5.jpg";
+import image from '@/public/assets/images/image1.jpg';
+import imag2 from '@/public/assets/images/image2.jpg';
+import imag3 from '@/public/assets/images/image3.jpg';
+import imag4 from '@/public/assets/images/image4.jpg';
+import imag5 from '@/public/assets/images/image5.jpg';
+
 
 const ImageSlider = () => {
-  const [positionIndex, setPositionIndex] = useState([0, 1, 2, 3, 4]);
 
-  const handleNext = () => {
-    //@ts-ignore
-    setPositionIndex((prevIndex) => (prevIndex + 1) % 5);
-  };
+    const [positonIndexs, setPositonIndexs] = useState([0, 1, 2, 3, 4]);
 
-  const images = [image, imag2, imag3, imag4, imag5];
 
-  const imageVariants = {
-    center: { x: "0%", scale: 1, zIndex: 5 },
-    left1: { x: "-50%", scale: 0.7, zIndex: 2 },
-    left: { x: "-90%", scale: 0.5, zIndex: 1 },
-    right: { x: "90%", scale: 0.5, zIndex: 1 },
-    right1: { x: "50%", scale: 0.7, zIndex: 2 },
-  };
+    const handelNext = () => {
+        setPositonIndexs((preIndex) => {
+            const updatedIndex = preIndex.map((preIndex) => (preIndex +1) % 5);
+            return updatedIndex;
+        });
+    } 
+
+    const images = [
+        image,
+        imag2,
+        imag3,
+        imag4,
+        imag5
+    ]
+
+    const positions = [
+        'center',
+        'left1',
+        'left',
+        'right',
+        'right1',
+    ]
+
+    const imageVarience = {
+        center: {x: '0%', scale: 1, zIndex: 5},
+        left1: {x: '-50%', scale: 0.7, zIndex: 2},
+        left: {x: '-90%', scale: 0.5, zIndex: 1},
+        right: {x: '90%', scale: 0.5, zIndex: 1},
+        right1: {x: '50%', scale: 0.7, zIndex: 2},
+    }
 
   return (
-    <div className="flex flex-col items-center justify-center bg-[#662549] h-screen">
-      {images.map((image, index) => (
-        <motion.img
-          key={index}
-          //@ts-ignore
-          src={image.src}
-          className={`your-classes-here`}
-          variants={imageVariants}
-          initial="center"
-          //@ts-ignore
-          animate={{ position: positionIndex[index] }}
-          transition={{ duration: 0.9 }}
-          style={{ width: "50%", position: "absolute" }}
-        />
-      ))}
-
-      <button className="text-white mt-[700px] w-20 h-10 rounded-xl bg-[#451952]" onClick={handleNext}>
-        Next
-      </button>
+    <div className='flex flex-col items-center justify-start h-full w-full'>
+        {images.map((image, index) =>(
+            <motion.img
+            key={index}
+            src= {image.src}
+            alt= ''
+            className={'rounded-2xl'}
+            initial= 'center'
+            animate= {positions[positonIndexs[index]]}
+            variants={imageVarience}
+            transition={{duration: 0.8}}
+            style={{width: '40%', position: 'absolute'}}
+            />
+        ))}
+        <button className='text-black rounded-lg py-2 px-4 mt-[500px] bg-[#183D3D]' onClick={handelNext}>Next</button>
     </div>
-  );
-};
+    
+  )
+}
 
-export default ImageSlider;
+export default ImageSlider
